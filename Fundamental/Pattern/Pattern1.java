4e3650770aa4827507f485f02281ae783836acc0
Ananya2306 <playing230106@gmail.com>
2024-04-26 23:59:04 +0530

    abc

---
+++ Fundamental/Pattern/Pattern1.java
@@ -0,0 +1,39 @@
+
+/*
+ * 1
+ * 23
+ * 345
+ * 4567
+ * 56789
+ */
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		//this is no. of rows
+	for(int i=1;i<=n;i++) {
+		/*System.out.println(i);
+		for(int j=2;j<=n;j++) {
+			System.out.println(j);
+		} */
+		
+		int a = 1;
+		//print for num of column
+		/*for(int j=i;a<=i;) {
+	
+			System.out.print(j++);
+			a++;*/
+		int j = i;
+		while(a<=i) {
+			System.out.print(j);
+			j++;
+			a++;
+		}
+		System.out.println();
+	}
+	
+   }
+
+}

e7cf79fdca13059984b13c9f1adacdbe86687a15
Ananya2306 <playing230106@gmail.com>
2024-05-08 22:42:39 +0530

    New Function Added

---
+++ Fundamental/Functions/Fibonacci.java
@@ -0,0 +1,39 @@
+import java.util.*;
+/*
+ Create a function that determines whether a given number N belongs to the Fibonacci sequence. If N is found in the Fibonacci sequence, the function should return true; otherwise, it should return false.
+ Sample Input 1 :
+5
+Sample Output 1 :
+true
+Explanation :
+Fibonacci sequence begins 0, 1, 1, 2, 3, 5, ... and so on. Since 5 appears in the sequence.
+ */
+public class Fibonacci{
+    public static boolean checkMember(int n) {
+        int a=1,b=1,c=0,f=0;
+        for(int i=0;c<2*n;i++)
+        {
+            c=a+b;
+            if(n==c)
+                f=1;
+            a=b;
+            b=c;
+        }
+        if(f==1)
+        
+        return true;
+    
+    else
+        
+        return false;
+    }
+    public static void main(String args[]){
+        int n;
+        Scanner snr= new Scanner(System.in);
+        n=snr.nextInt();
+        snr.close();
+        System.out.println(checkMember(n));
+    }
+    
+}
+

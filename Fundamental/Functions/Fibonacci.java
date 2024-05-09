561f2b24acb71b3ed0777e6a18d1da76aa6a9930
Ananya2306 <playing230106@gmail.com>
2024-05-09 23:02:02 +0530

    Fibo Modify

--- Fundamental/Functions/Fibonacci.java
+++ Fundamental/Functions/Fibonacci.java
@@ -10,29 +10,23 @@ Fibonacci sequence begins 0, 1, 1, 2, 3, 5, ... and so on. Since 5 appears in th
  */
 public class Fibonacci{
     public static boolean checkNumber(int n) {
-        int a=1,b=1,c=0,f=0;
-        for(int i=0;c<2*n;i++)
-        {
-            c=a+b;
-            if(n==c)
-                f=1;
-            a=b;
-            b=c;
+        int n1=1,n2=1,n3;
+        while(n1<n){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
         }
-        if(f==1)
-        
+        if(n1==n){
         return true;
-    
-    else
-        
+        }else{
         return false;
+        }
     }
     public static void main(String args[]){
-        int n;
-        Scanner snr= new Scanner(System.in);
-        n=snr.nextInt();
-        snr.close();
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
         System.out.println(checkNumber(n));
+        sc.close();
     }
     
 }

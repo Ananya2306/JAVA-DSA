2c22cb1826bd19b76632f76e4b0af9ad389398cd
Ananya2306 <playing230106@gmail.com>
2024-05-11 19:56:38 +0530

    Just Added que

---
+++ Fundamental/Arrays/LinearSearch.java
@@ -0,0 +1,32 @@
+public class LinearSearch {
+    /*
+You have been given a random integer array/list(ARR) of size N, and an integer X. 
+You need to search for the integer X in the given array/list using 'Linear Search'.
+You have been required to return the index at which X is present in the array/list. 
+If X has multiple occurrences in the array/list, 
+then you need to return the index at which the first occurrence of X would be encountered. 
+In case X is not present in the array/list, then return -1.
+
+'Linear search' is a method for finding an element within an array/list. 
+It sequentially checks each element of the array/list until a match is found or the whole array/list has been searched.'
+Sample Input 1:
+7
+2 13 4 1 3 6 28
+3
+Sample Output 1:
+4
+You have been given an empty array(ARR) and its size N. 
+The only input taken from the user will be N and you need not worry about the array.
+
+Your task is to populate the array using the integer values 
+in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
+You need not print the array. You only need to populate it.
+Sample Input 1 :
+1
+6
+Sample Output 1 :
+1 3 5 6 4 2
+Explanation of Sample Input 1 :
+Since the value of N is 6, the number will be stored in the array in such a fashion that 1 will appear at 0th index, then 2 at the last index, in a similar fashion 3 is stored at index 1. Hence the array becomes 1 3 5 6 4 2.
+     */
+}

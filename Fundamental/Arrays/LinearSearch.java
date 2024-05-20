6447cf474af06134d7965b73cbd1413bbb960110
Ananya2306 <playing230106@gmail.com>
2024-05-20 22:51:41 +0530

    Linear Search

--- Fundamental/Arrays/LinearSearch.java
+++ Fundamental/Arrays/LinearSearch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinearSearch {
     /*
 You have been given a random integer array/list(ARR) of size N, and an integer X. 
@@ -15,18 +17,36 @@ Sample Input 1:
 3
 Sample Output 1:
 4
-You have been given an empty array(ARR) and its size N. 
-The only input taken from the user will be N and you need not worry about the array.
+ */
 
-Your task is to populate the array using the integer values 
-in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
-You need not print the array. You only need to populate it.
-Sample Input 1 :
-1
-6
-Sample Output 1 :
-1 3 5 6 4 2
-Explanation of Sample Input 1 :
-Since the value of N is 6, the number will be stored in the array in such a fashion that 1 will appear at 0th index, then 2 at the last index, in a similar fashion 3 is stored at index 1. Hence the array becomes 1 3 5 6 4 2.
-     */
+public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size of array:- ");
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i < n;i++) {
+			arr[i] = s.nextInt();
+		}
+
+        s.close();
+		return arr;
+	}
+    public static int linearSearch(int[] arr, int x) {
+        for(int i = 0;i < arr.length;i++) {
+            if(arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the element to be searched:- ");
+        int x = s.nextInt();
+        int arr[] = takeInput();
+        System.out.println(linearSearch(arr, x));
+        s.close();
+    }
 }
---
+++ Fundamental/Arrays/Populate.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Populate {
+/*
+You have been given an empty array(ARR) and its size N. 
+The only input taken from the user will be N and you need not worry about the array.
+
+Your task is to populate the array using the integer values 
+in the range 1 to N(both inclusive) in the order - 1,3,5,.......,6,4,2.
+You need not print the array. You only need to populate it.
+
+Sample Input 1 :
+1
+6
+Sample Output 1 :
+1 3 5 6 4 2
+
+Explanation of Sample Input 1 :
+Since the value of N is 6,
+the number will be stored in the array 
+in such a fashion that 1 will appear at 0th index, 
+then 2 at the last index, 
+in a similar fashion 3 is stored at index 1. 
+Hence the array becomes 1 3 5 6 4 2.
+   
+*/
+    public static void populate(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+
+                arr[i] = i + 1;
+            } else {
+                
+                arr[i] = arr.length - i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        populate(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}

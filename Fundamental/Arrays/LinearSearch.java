93d253c649c5387894fa44048c796ca8475a34ea
Ananya2306 <playing230106@gmail.com>
2024-05-23 21:09:41 +0530

    Starting

--- Fundamental/Arrays/LinearSearch.java
+++ Fundamental/Arrays/LinearSearch.java
@@ -19,7 +19,7 @@ Sample Output 1:
 4
  */
 
-public static int[] takeInput() {
+ public static int[] takeInput() {
 		Scanner s = new Scanner(System.in);
         System.out.print("Enter the size of array:- ");
 		int n = s.nextInt();
---
+++ Fundamental/Arrays/SecLargest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SecLargest {
+    public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+        System.out.print("Enter the size of array:- ");
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i < n;i++) {
+			arr[i] = s.nextInt();
+		}
+
+        s.close();
+		return arr;
+	}
+}

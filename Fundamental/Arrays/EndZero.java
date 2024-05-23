e3843220185f0ac3424ea20a659e4b7ce0dd3b9e
Ananya2306 <playing230106@gmail.com>
2024-05-23 21:18:40 +0530

    Done

--- Fundamental/Arrays/EndZero.java
+++ Fundamental/Arrays/EndZero.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ */
 public class EndZero {
     public static int[] takeInput() {
 		Scanner s = new Scanner(System.in);
@@ -13,4 +15,25 @@ public class EndZero {
         s.close();
 		return arr;
 	}
+
+    public static void endZero(int[] arr) {
+        int a=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                int c = arr[a];
+                arr[a] =arr[i];
+                arr[i] = c;
+                a++;    
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        endZero(arr);
+    }
 }
--- Fundamental/Arrays/SecLargest.java
+++ Fundamental/Arrays/SecLargest.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/*
+ Given an array Arr of size N, print the second largest distinct element from an array.
+If the second largest element doesn't exist then return -1.
+ */
 public class SecLargest {
     public static int[] takeInput() {
 		Scanner s = new Scanner(System.in);
@@ -13,4 +16,32 @@ public class SecLargest {
         s.close();
 		return arr;
 	}
+    public static int secondLargest(int[] arr) {
+        int a=0,b=0;
+        for(int i=0;i<arr.length;i++){
+            int c=0;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    c++;
+                }
+            }
+            if(c==0){
+                if(arr[i]>a){
+                    b=a;
+                    a=arr[i];
+                }
+                else if(arr[i]>b){
+                    b=arr[i];
+                }
+            }
+        }
+        if(b==0){
+            return -1;
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        System.out.println(secondLargest(arr));
+        }
 }

419db9e2c4a86515ebfceab79b244b914b7603e2
Ananya2306 <playing230106@gmail.com>
2024-06-10 23:07:32 +0530

    Sum

---
+++ Recursion/Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static int Sums(int n){
+        if(n==0){
+           // System.out.println(sum);
+            return 0;
+        }
+        int ans = Sums(n-1);
+        return ans+=n;
+       // System.out.println(ans);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number :-");
+        int n = sc.nextInt();
+        System.out.println(Sums(n));
+        sc.close();
+    }
+}

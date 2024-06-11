57b1f169aab9dc5d3fac458f868b56c40588a37b
Ananya2306 <playing230106@gmail.com>
2024-06-11 23:18:53 +0530

    Removal character

---
+++ Recursion/RemoveChar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+//Given a string, compute recursively a new string where all 'x' chars have been removed.
+public class RemoveChar {
+    public static String Remove(String str){
+        if(str.length()==0){
+            return "";
+        }
+        if(str.charAt(0)=='x'){
+            return Remove(str.substring(1));
+        }
+        return str.charAt(0)+Remove(str.substring(1));
+
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string :- ");
+        String str = sc.nextLine();
+        System.out.println(Remove(str));
+        sc.close();
+    }
+}

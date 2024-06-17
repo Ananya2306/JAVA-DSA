112a6461da595cd35847b4b28db343005011cbe0
Ananya2306 <playing230106@gmail.com>
2024-06-17 23:14:49 +0530

    Identical characters

---
+++ Recursion/IdenticalChars.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class IdenticalChars {
+    public static String Identical(String str){
+        if(str.length()<=1){
+            return str;
+        }
+        if(str.charAt(0)==str.charAt(1)){
+            return str.charAt(0)+"*"+Identical(str.substring(1));
+        }else{
+            return str.charAt(0)+Identical(str.substring(1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string :- ");
+        String str = sc.next();
+        System.out.println(Identical(str));
+        sc.close();
+    }
+}

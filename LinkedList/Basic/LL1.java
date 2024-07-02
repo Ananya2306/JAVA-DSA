6ee6f94a40c2e29be983f2271ff344a32548a70e
Ananya2306 <playing230106@gmail.com>
2024-07-02 23:20:02 +0530

    New way LL

---
+++ LinkedList/Basic/LL1.java
@@ -0,0 +1,33 @@
+package LinkedList.Basic;
+class Node<T> {
+    T data;
+    Node<T> next;
+    Node(T data){
+        this.data = data;
+    }
+}
+public class LL1 {  
+        public static void print(Node<Integer> head) {
+            Node<Integer> temp = head;
+            while(temp!=null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+        
+        public static void main(String[] args) {
+            Node<Integer> node1 = new Node<Integer>(10);
+            Node<Integer> node2 = new Node<Integer>(20);
+            Node<Integer> node3 = new Node<Integer>(30);
+            Node<Integer> node4 = new Node<Integer>(40);
+            Node<Integer> node5 = new Node<Integer>(50);
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = node4;
+            node4.next = node5;
+            print(node1);
+        }
+    
+    }
+    
